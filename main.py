6a394a94283cb0f0e6b457340bd634846b81e425
Fernando Di Masi <wildiney@gmail.com>
2021-06-30 14:16:22 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+from datetime import datetime
+from dotenv import load_dotenv
+load_dotenv(dotenv_path='./.env')
+
+
+def main():
+    date = datetime.now()
+    video_folder = os.environ.get('VIDEO_FOLDER')
+    date_video = str(date.strftime("%Y")) + "-" + str(date.strftime("%m"))
+    video_url = input("Digite a URL do vídeo no Youtube: ")
+
+    os.system(
+        'youtube-dl {0} -o {1}/{2}-%(title)s.%(ext)s'.format(video_url, video_folder, date_video))
+
+
+if __name__ == '__main__':
+    main()
